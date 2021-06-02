95b323f75c8ce4f830ada52524b57f561cbc49f5
ashuyad98 <31366144+ashuyad98@users.noreply.github.com>
2021-06-03 02:47:59 +0530

    Create DKHCP.py

---
+++ python/DKHCP.py
@@ -0,0 +1,14 @@
+#this program is used when hcp parameter 'a','c' is also temp. dependence.
+#Programe for calculate temperature dependent soec and toec,VELOCITY
+# attenuation, along Z-AXIS for hcp using different data of c33  .
+
+
+import math
+
+
+
+
+
+if __name__ == "__main__":
+  
+    main()
