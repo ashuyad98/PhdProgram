650cf72d317318fa7354f29d480a989db55161f3
ashuyad98 <31366144+ashuyad98@users.noreply.github.com>
2021-06-06 02:00:12 +0530

    Update DKHCP.py

--- python/DKHCP.py
+++ python/DKHCP.py
@@ -91,7 +91,6 @@ def main():
     L[i]=C[i]/(A[i]+C[i])
     M[i]=A[i]/(A[i]+C[i])
     XX[i]=A[i]*C[i]
-    
     if XX[i]<0:
       AC[i]=-XX[i]
     else:
@@ -99,7 +98,58 @@ def main():
       
     #This is formulation for Gruneisen constant.
     #along theeta zero i.e. for longitudinal wave.
+    x1[i]=1.0+( (c13[i]+c113[i])/(2.0*c33[i]) );
+    x2[i]=(c13[i]+c113[i])/(2.0*c11[i]);
+    x3[i]=(c13[i]+c344[i])/(2.0*c44[i]);
+    x4[i]=(c13[i]+0.5*(c113[i]-c123[i]) )/(2.0*c44[i]);
+    x5[i]=1.0+( (c13[i]+c344[i])/(2.0*c44[i]) );
+    x6a[i]=((A[i]+C[i])*c13[i])/(2.0*(C[i]*c11[i]+A[i]*(2.0*c44[i]+c13[i])));
+    x6b[i]=( pow(C[i],2.0)*c113[i]+pow(A[i],2.0)*c333[i]+2.0*A[i]*C[i]*(c133[i]+2.0*c344[i]))/pow((A[i]+C[i]), 3.0);
+    x6[i]=M[i]+x6a[i]+x6b[i];
+    x7[i]=( (A[i]+C[i])*c13[i]+0.5*C[i]*(c113[i]-c123[i])+A[i]*c344[i] )/(C[i]*(c11[i]-c12[i])+2.0*A[i]*c44[i]);
+    x8a[i]=(A[i]+C[i])/( 2.0*(C[i]*c44[i]+A[i]*(c33[i]-c44[i]-c13[i])) );
+    x8b[i]=c13[i]+(A[i]*C[i]/pow( (A[i]+C[i]), 2.0))*(c113[i]+c333[i]-c113[i]);
+    x8c[i]=pow( ((C[i]-A[i])/(C[i]+A[i])),2.0)*c344[i];
+    x8[i]=M[i]+x8a[i]*(x8b[i]+x8c[i]);
+    x[i]=(x1[i]*x1[i]+x2[i]*x2[i]+x3[i]*x3[i]+x4[i]*x4[i]+x5[i]*x5[i]+x6[i]*x6[i]+x7[i]*x7[i]+x8[i]*x8[i])/41.0;
+    xav1[i]=(x1[i]+x2[i]+x3[i]+x4[i]+x5[i]+x6[i]+x7[i]+x8[i])/41.0;
+    xav[i]=pow(xav1[i],2.0);
     
+    #This is formulation for Gruneisen constant.
+    #along theeta zero i.e. for shear wave.
+    y1a[i]=(A[i]*(c44[i]+2.0*c344[i])+C[i]*(c44[i]+2.0*c155[i]))/(C[i]*c11[i]+A[i]*(2.0*c44[i]+c13[i]));
+    y1[i]=L[i]*M[i]*(1+y1a[i]);
+    y2[i]=-L[i]*M[i]*(1+y1a[i]);
+    y3a[i]=(A[i]*(c44[i]+2.0*c344[i])+C[i]*(c44[i]+2.0*c155[i]))/(C[i]*c11[i]+A[i]*(2.0*c44[i]+c13[i]));
+    y3[i]=K*L[i]*M[i]*(1+y3a[i]);
+    y4[i]=-K*L[i]*M[i]*(1+y3a[i]);
+    y5a[i]=(c155[i]+2.0*c44[i]-c144[i])/(C[i]*(c11[i]-c12[i])+2.0*A[i]*c44[i]);
+    y5[i]=-pow(AC[i],0.5)*y5a[i];
+    y6[i]=pow(AC[i],0.5)*y5a[i];
+    y7a[i]=( (A[i]-C[i])*(c344[i]-c155[i])-(A[i]+C[i])*c44[i])/(C[i]*c44[i]+A[i]*(c33[i]-c44[i]-c13[i]));
+    y7[i]=-(pow(AC[i],0.5)/(A[i]+C[i]))*(1+y7a[i]);
+    y8[i]=(pow(AC[i],0.5)/(A[i]+C[i]))*(1+y7a[i]);
+    y9[i]=-(1.0/1.414)*(pow(AC[i],0.5)/(A[i]+C[i]))*(1+y7a[i]);
+    y10[i]=(1.0/1.414)*(pow(AC[i],0.5)/(A[i]+C[i]))*(1+y7a[i]);
+    y[i]=(y1[i]*y1[i]+y2[i]*y2[i]+y3[i]*y3[i]+y4[i]*y4[i]+y5[i]*y5[i]+y6[i]*y6[i]+y7[i]*y7[i]+y8[i]*y8[i]+y9[i]*y9[i]+y10[i]*y10[i])/16.0;
+    yav1[i]=(y1[i]+y2[i]+y3[i]+y4[i]+y5[i]+y6[i]+y7[i]+y8[i]+y9[i]+
+    y10[i])/16.0;
+    yav[i]=pow(yav1[i],2.0);
+    
+    #calculation of attenuation
+    Vl[i]=sqrt(c33[i]/dd[i]);
+    Vs[i]=sqrt(c44[i]/dd[i]);
+    V[i]=pow(3,0.333)*pow((1/pow(Vl[i],3)+2/pow(Vs[i],3)),-0.333);
+    Cv[i]=Cva[i]*pow(10,Cvb[i]);
+    Eo[i]=Eoa[i]*pow(10,Eob[i]);
+    ko[i]=koa[i]*pow(10,kob[i]);
+    to[i]=3*ko[i]/(Cv[i]*V[i]*V[i]);
+    tl[i]=2*to[i]; ts[i]=to[i];
+    Dl[i]=9*x[i]-( (3*xav[i]*Cv[i]*T[i])/Eo[i]);
+    Ds[i]=9*y[i];
+    ath[i]=(4.0*3.14*3.14*xav[i]*ko[i]*T[i])/(2*dd[i]*pow(Vl[i],5));
+    al[i]=4*3.14*3.14*tl[i]*Eo[i]*Dl[i]/(3*dd[i]*pow(Vl[i],3));
+    as[i]=4*3.14*3.14*ts[i]*Eo[i]*Ds[i]/(3*dd[i]*pow(Vs[i],3));
     
 if __name__ == "__main__":
   
